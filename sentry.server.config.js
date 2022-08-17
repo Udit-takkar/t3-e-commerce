cfc2b992bfb6b7c915741471cc04eedb9659676a
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-08-18 00:04:32 +0530

    infra: sentry added icons, form,font added

---
+++ sentry.server.config.js
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...a1c4d3420192407191f04b23e9c836ca@o1362755
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,47 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      addBtn: "rgb(20, 110, 180)",
+      cnt: "#146eb41a",
+      primary: {
+        blue: "#2874f0",
+        lightGreen: "#14be47",
+        green: "#388e3c",
+        yellow: "#ff9f00",
+        orange: "#fb641b",
+        darkBlue: "#172337",
+        grey: "#878787",
+      },
+    },
+    extend: {
+      fontFamily: {
+        jakarta: ["jakarta", "sans-serif"],
+        helvetica: ["helvetica", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        products: "repeat(auto-fill, minmax(250px, 1fr))",
+        productItem: "90px 1fr",
+      },
+    },
   },
   plugins: [],
 };
